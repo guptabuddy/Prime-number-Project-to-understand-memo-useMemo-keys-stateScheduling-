5c87a179c0d383628f3a1dd6b0357b7c554d5462
guptabuddy <guptabudy@gmail.com>
2025-11-21 23:21:04 +0530

    Change in Counter comp. state to also contain the ID for each and every counter change value. This ID will be used to render the list items.

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -5,9 +5,11 @@ import MinusIcon from "../UI/Icons/MinusIcon.jsx";
 import PlusIcon from "../UI/Icons/PlusIcon.jsx";
 import CounterOutput from "./CounterOutput.jsx";
 import { log } from "../../log.js";
+import CounterHistory from "./CounterHistory.jsx";
 
 function isPrime(number) {
 	log("Calculating if is prime number", 2, "other");
+
 	if (number <= 1) {
 		return false;
 	}
@@ -25,18 +27,24 @@ function isPrime(number) {
 
 const Counter = memo(function Counter({ initialCount }) {
 	log("<Counter /> rendered", 1);
-	const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
-	// This above useMemo() hook will prevent the unnecessary execution of the isPrime() function. It will only execute the function if the initialCount value changes.
-	// This will be helpful in situations where there is a lot of code being executed inside the function, and that function is being unnecessarily executed.
 
-	const [counter, setCounter] = useState(initialCount);
+	const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
+
+	// const [counter, setCounter] = useState(initialCount);
+
+	// const [counterChanges, setCounterChanges] = useState([initialCount]);
+	const [counterChanges, setCounterChanges] = useState([{ value: initialCount, id: Math.random() * 1000 }]);
+
+	const currentCounter = counterChanges.reduce((prevCounter, counterChange) => prevCounter + counterChange.value, 0);
 
 	const handleDecrement = useCallback(function handleDecrement() {
-		setCounter((prevCounter) => prevCounter - 1);
+		// setCounter((prevCounter) => prevCounter - 1);
+		setCounterChanges((prevCounterChanges) => [{ value: -1, id: Math.random() * 1000 }, ...prevCounterChanges]);
 	}, []);
 
 	const handleIncrement = useCallback(function handleIncrement() {
-		setCounter((prevCounter) => prevCounter + 1);
+		// setCounter((prevCounter) => prevCounter + 1);
+		setCounterChanges((prevCounterChanges) => [{ value: 1, id: Math.random() * 1000 }, ...prevCounterChanges]);
 	}, []);
 
 	return (
@@ -49,11 +57,12 @@ const Counter = memo(function Counter({ initialCount }) {
 				<IconButton icon={MinusIcon} onClick={handleDecrement}>
 					Decrement
 				</IconButton>
-				<CounterOutput value={counter} />
+				<CounterOutput value={currentCounter} />
 				<IconButton icon={PlusIcon} onClick={handleIncrement}>
 					Increment
 				</IconButton>
 			</p>
+			<CounterHistory history={counterChanges} />
 		</section>
 	);
 });
--- src/components/Counter/CounterHistory.jsx
+++ src/components/Counter/CounterHistory.jsx
@@ -21,10 +21,26 @@ function HistoryItem({ count }) {
 export default function CounterHistory({ history }) {
 	log("<CounterHistory /> rendered", 2);
 
+	// const [selected, setSelected] = useState(false);
+
+	// function handleClick() {
+	// 	setSelected((prevSelected) => !prevSelected);
+	// }
+
 	return (
 		<ol>
-			{history.map((count, index) => (
-				<HistoryItem key={index} count={count} />
+			{/* {history.map((count, index) => (
+				<HistoryItem key={index} count={count} /> */}
+			{history.map((count) => (
+				<HistoryItem key={count.id} count={count.value} />
+
+				// <li key={index} onClick={handleClick} className={selected ? "selected" : undefined}>
+				// 	{count}
+				// </li>
+				// If we do like the above way, with state and handleClick function inside this component function, then what's happening is that the "selected" css class is getting applied to all the list items, which is not what we want.
+
+				// The reason the class is added to all list items is that the variable selected is likely a single piece of state (a boolean like true or false) that is shared by the entire component.
+				// When the component re-renders, every single item in the map loop looks at that same selected variable. If it is true, every <li> gets the class.
 			))}
 		</ol>
 	);
