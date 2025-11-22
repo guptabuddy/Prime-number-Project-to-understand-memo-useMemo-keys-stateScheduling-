19b11d19a6fed2ce1eb13a68aaf911d6d399e475
guptabuddy <guptabudy@gmail.com>
2025-11-22 22:59:36 +0530

    Add Million.js to make the App faster

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// After using the Million.js (L-217)
+import million from "million/compiler";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [million.vite({ auto: true }), react()],
+});
